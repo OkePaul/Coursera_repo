e5f3593c2870922063d532f105db7736df992855
OkePaul <57877724+OkePaul@users.noreply.github.com>
2022-12-04 13:17:09 +0100

    Add files via upload

---
+++ mod4/script.js
@@ -0,0 +1,30 @@
+// function multiply(x, y){
+//     return x * y;
+// }
+// console.log(multiply(5, 2));
+
+
+// var named = "Paul";
+// if (named = "Paul"){
+//     console.log("Hello Paul")
+// }
+
+
+let selectedNames = ['Paul', 'Chinedu', 'Kelvin'];
+console.log(selectedNames);
+console.log("Hello" + " " + selectedNames[0]);
+// if (selectedNames = 0){
+//     console.log("hello Paul")
+// }
+
+
+
+// console.getString = named;
+// function getNames(){
+    
+//     if (named = "Paul"){
+       
+        
+//     }
+// }
+// console.log("Hello Paul")
